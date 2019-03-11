fcda79b8d9ebe28008a651eeb393ab698244d375
zhangsan <zhangsan@itcast.cn>
2019-03-11 18:21:23 +0800

    修改FristClass

--- src/com/itheima/git/FirstClass.java
+++ src/com/itheima/git/FirstClass.java
@@ -8,6 +8,7 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        //hello test
     }
     public void test(){
         int i = 0;
